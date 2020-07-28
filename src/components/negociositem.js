fe30dea67ab8d3c09afc7f67dabb8ce501eb4c56
alexzaga <alexzaga@gmail.com>
2020-07-27 19:59:10 -0500

    v0.9.5-alpha

--- src/components/negociositem.js
+++ src/components/negociositem.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const NegociosItem = ({ obj, id }) => {
     let _timer = obj.typeOfBusiness === "Hospedaje" ? (<li><strong>Entrada-Salida:</strong>&nbsp;{obj.checkinout}</li>) : (<li><strong>Servicio:</strong>&nbsp;{obj.openclose}</li>);
     return(
-        <div className="cardd" onClick={(e) => console.log(e.target.id)}>
+        <div className="cardd">
             <div className="cardd-inner">
                 <div className="cardd-front">
-                    <img src={obj.image} alt="" />
+                    <img src={obj.image} alt=""/>
                 </div>
                 <div className="cardd-back">
-                    <h1>{obj.name}</h1>
-                    <ul id={id}>
+                    <h1>
+                        {obj.name}&nbsp;&nbsp;<Button name={obj.typeOfBusiness} id={id} onClick={(e) => sessionStorage.setItem(e.target.name, e.target.id)} size="sm" variant="outline-info">Seleccionar</Button>
+                    </h1>
+                    <ul>
                         <li><strong>Direcci&oacute;n:</strong>&nbsp;{obj.address}</li>
                         <li><strong>Correo:</strong>&nbsp;{obj.email}</li>
                         <li><strong>Informaci&oacute;n:</strong>&nbsp;{obj.description}</li>
